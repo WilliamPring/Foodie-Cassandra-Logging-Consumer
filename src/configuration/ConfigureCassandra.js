68c86421f9b1146b68db033c1c2566b62a92ebef
WilliamPring <mrwilliampring@gmail.com>
2020-05-07 00:08:09 -0400

    try catch on insert

--- src/configuration/ConfigureCassandra.js
+++ src/configuration/ConfigureCassandra.js
@@ -4,7 +4,8 @@ import {values} from 'lodash'
 export default class ConfigureCassandra {
 
     constructor() {
-        let cassandraConfig = config.get('Cassandra').config;
+        const cassandraConfig = config.get('Cassandra').config;
+
         console.log(cassandraConfig)
         let authProvider = new auth.PlainTextAuthProvider('cassandra', 'password123');
         this._client = new Client({
@@ -14,15 +15,18 @@ export default class ConfigureCassandra {
     }
 
     async logMessage(message) {
-        const log_id = types.TimeUuid.now();
-        message[log_id] = log_id;
-        console.log(message)
-        const data = values(message)
-        console.log(data)
-        await this._client.execute('INSERT INTO foodie.logs (log_application, log_type, log_application_action, log_message, log_timestamp, log_id) VALUES (?, ?, ?, ?, ?, ?)',
-                                    data, { prepare: true });
-        console.log('Row updated on the cluster');
+        try {
 
+
+            const log_id = types.TimeUuid.now();
+            message[log_id] = log_id;
+            const data = values(message)
+            console.log(data)
+            await this._client.execute('INSERT INTO foodie.logs (log_application, log_type, log_application_action, log_message, log_timestamp, log_id) VALUES (?, ?, ?, ?, ?, ?)',
+                                        data, { prepare: true });
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     async getClinet() {
